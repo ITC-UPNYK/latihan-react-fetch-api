69c545b152453b2a888d5852c704def42d91d5c6
Haikal Mumtaz <123210062@student.upnyk.ac.id>
2024-05-03 21:32:19 +0700

    nambahin headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
@@ -8,6 +8,8 @@ function App() {
   const [judul, setJudul] = useState('');
   // State untuk menyimpan isi catatan yang sedang ditulis
   const [isi, setisi] = useState('');
+  // State untuk menyimpan pesan respons dari server
+  const [responseMessage, setResponseMessage] = useState('');
 
   // Mengambil daftar catatan dari server saat komponen dimuat atau diperbarui
   useEffect(() => {
@@ -18,56 +20,85 @@ function App() {
   const kirimCatatan = async (e) => {
     e.preventDefault(); // Mencegah perilaku default form submit
 
-    try{
-      await axios.post('http://localhost:3002/v1/todo', {
-        judul, isi
+    try {
+      const response = await axios.post('http://localhost:3002/v1/todos_auth', {
+        judul,
+        isi
+      }, {
+        headers: {
+          Authorization: 'Bearer a1b2c3d4e5f6'
+        }
       }); // Mengirim data catatan baru ke server
       setJudul(''); // Mengosongkan judul setelah mengirim catatan
       setisi(''); // Mengosongkan isi setelah mengirim catatan
       ambilCatatan(); // Memperbarui daftar catatan setelah mengirim catatan baru
+      setResponseMessage({ message: response.data.message, bgColor:'bg-green-200', textColor:'text-green-800'}); // Menyimpan pesan respons dari server
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
 
   // Fungsi untuk mengambil daftar catatan dari server
   const ambilCatatan = async () => {
     try {
-      const response = await axios.get('http://localhost:3002/v1/todo'); // Mengambil data catatan dari server
+      const response = await axios.get('http://localhost:3002/v1/todos_auth', {
+        headers: {
+          Authorization: 'Bearer a1b2c3d4e5f6'
+        }
+      }); // Mengambil data catatan dari server
       setCatatan(response.data.data); // Menyimpan data catatan ke dalam state
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
-  }
+  };
 
   // Fungsi untuk menghapus catatan berdasarkan ID
   const hapusCatatan = async (id) => {
     try {
-      await axios.delete(`http://localhost:3002/v1/todo/${id}`); // Menghapus catatan dari server berdasarkan ID
+      const response = await axios.delete(`http://localhost:3002/v1/todos_auth/${id}`, {
+        headers: {
+          Authorization: 'Bearer a1b2c3d4e5f6'
+        }
+      }); // Menghapus catatan dari server berdasarkan ID
       ambilCatatan(); // Memperbarui daftar catatan setelah menghapus catatan
+      setResponseMessage({ message: response.data.message, bgColor:'bg-red-200', textColor:'text-red-800'}); // Menyimpan pesan respons dari server dengan background merah
     } catch (error) {
       console.error(error.message);
     }
-  }
+  };
 
   return (
     <div className="max-w-md mx-auto mt-5">
       <h1 className="text-2xl font-bold mb-4">To-Do-List</h1>
+      {responseMessage.message && (
+        <div className={`${responseMessage.bgColor} ${responseMessage.textColor} py-2 px-4 mb-4 rounded-md flex justify-between items-center`}>
+          <span>{responseMessage.message}</span>
+          <button onClick={() => setResponseMessage("")} className='text-sm text-gray-600 focus:outline-none ml-2'>&times;</button>
+        </div>
+      )}
       <form className="mb-4" onSubmit={kirimCatatan}>
         {/* Input untuk judul catatan */}
-        <input type="text" className="w-full mb-2 px-3 py-4 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" 
+        <input
+          type="text"
+          className="w-full mb-2 px-3 py-4 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
           placeholder="Judul"
           value={judul}
           onChange={(e) => setJudul(e.target.value)}
         />
         {/* Input untuk isi catatan */}
-        <textarea className="w-full mb-2 px-3 py-4 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+        <textarea
+          className="w-full mb-2 px-3 py-4 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
           placeholder="Isi Catatan"
           onChange={(e) => setisi(e.target.value)}
           value={isi}
         ></textarea>
         {/* Tombol untuk menambahkan catatan */}
-        <button type="submit" className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-red-600">Add</button>
+        <button
+          type="submit"
+          className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-red-600"
+        >
+          Add
+        </button>
       </form>
       <div>
         {/* Menampilkan daftar catatan */}
@@ -76,14 +107,17 @@ function App() {
             <h2 className="text-xl font-semibold mb-2">{catatan.judul}</h2>
             <p className="text-gray-700 mb-2">{catatan.isi}</p>
             {/* Tombol untuk menghapus catatan */}
-            <button className="bg-red-500 text-white py-1 px-3 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600"
+            <button
+              className="bg-red-500 text-white py-1 px-3 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600"
               onClick={() => hapusCatatan(catatan.id)}
-            >Delete</button>
+            >
+              Delete
+            </button>
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
